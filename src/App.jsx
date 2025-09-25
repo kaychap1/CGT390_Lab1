156ad1f01f4a553b7bda963810bda7251f86e55d
kaysenchapman <kaysenchapman@gmail.com>
2025-09-25 23:57:39 +0000

    lab 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header.jsx';
 import Card from './components/Card.jsx';
@@ -11,6 +12,15 @@ import Dropdown from './components/Dropdown.jsx';
 import Search from './components/Search.jsx';
 import AddProfile from './components/AddProfile.jsx';
 import FetchedCardsSection from './components/FetchedCardsSection.jsx';
+
+function NotFound() {
+  return (
+    <Wrapper>
+      <h2>Page Not Found</h2>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </Wrapper>
+  );
+}
 
 function App() {
   const [selectedPosition, setSelectedPosition] = useState('All');
@@ -46,31 +56,36 @@ function App() {
     <>
       <Wrapper>
         <Header />
-      </Wrapper>
-      <Wrapper>
-        <Intro />
-      </Wrapper>
-      <Wrapper>
-        <AddProfile onAddProfile={handleAddProfile} />
-      </Wrapper>
-      <Wrapper>
-        {/* Pass the correct handlers to the components */}
-        <Search onSearch={handleNameSearch} />
-        <Dropdown onSelect={handlePositionSelect} options={['Leader', 'Engineer']} />
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </nav>
       </Wrapper>
       
-      <div>
-        {filteredProfiles.map((profile, index) => (
-          <Wrapper key={index}>
-            <Card
-              name={profile.name}
-              description={profile.description}
-              image={profile.image}
-            />
-          </Wrapper>
-        ))}
-      </div>
-      <FetchedCardsSection />
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Wrapper>
+              <Search onSearch={handleNameSearch} />
+              <Dropdown onSelect={handlePositionSelect} options={['Leader', 'Engineer']} />
+            </Wrapper>
+            <div>
+              {filteredProfiles.map((profile, index) => (
+                <Wrapper key={index}>
+                  <Card
+                    name={profile.name}
+                    description={profile.description}
+                    image={profile.image}
+                  />
+                </Wrapper>
+              ))}
+            </div>
+            <FetchedCardsSection />
+          </>
+        } />
+        <Route path="/about" element={<Intro />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
